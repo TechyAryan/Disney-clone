d4b79a060c07648ba75593c414b8cfda12bda6a3
Aryan Mittal <aryanmittal961@gmail.com>
2025-02-21 18:05:07 +0530

    tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react(), tailwindcss()],
   server: {
-    historyApiFallback: true, 
+    headers: {
+
+    },
   },
 });
